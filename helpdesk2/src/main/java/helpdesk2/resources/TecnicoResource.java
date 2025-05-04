dcfedf6918de88cf2743c4b4c891c3ea0caeeec9
marcus aurelio <marcusaurelio743@gmail.com>
2025-05-03 22:57:49 -0300

    criando rest controler de tecnico

---
+++ helpdesk2/src/main/java/helpdesk2/resources/TecnicoResource.java
@@ -0,0 +1,26 @@
+package helpdesk2.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import helpdesk2.domains.Tecnico;
+import helpdesk2.services.TecnicoService;
+
+@RestController
+@RequestMapping(value = "/tecnicos")
+public class TecnicoResource {
+	@Autowired
+	private TecnicoService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Tecnico> findById(@PathVariable Long id){
+		Tecnico tecnico = service.findById(id);
+		
+		return ResponseEntity.ok().body(tecnico);
+	}
+
+}
--- helpdesk2/src/main/java/helpdesk2/services/DBService.java
+++ helpdesk2/src/main/java/helpdesk2/services/DBService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import helpdesk2.domains.Chamado;
 import helpdesk2.domains.Cliente;
 import helpdesk2.domains.Tecnico;
+import helpdesk2.domains.enumns.Perfil;
 import helpdesk2.domains.enumns.Prioridade;
 import helpdesk2.domains.enumns.Status;
 import helpdesk2.repositores.ChamadoRepository;
@@ -25,6 +26,8 @@ public class DBService {
 	
 	public void InstanciaDB() {
 		Tecnico tec1 = new Tecnico(null, "Marcus Aurelio", "9098766656", "marcusTeste@email.com", "admin");
+		tec1.AddPerfis(Perfil.ADMIN); 
+		
 		Cliente cli1 = new Cliente(null, "jose", "9088776473", "cliente@email.com", "admin");
 		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado1", "Primeiro chamado", tec1, cli1);
 		tecnicoRepository.saveAll(Arrays.asList(tec1));
