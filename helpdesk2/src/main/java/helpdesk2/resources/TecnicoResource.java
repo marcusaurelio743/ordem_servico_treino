f6f0ceff4e2a8d539544f26bc7f4c26c7e617295
marcus aurelio <marcusaurelio743@gmail.com>
2025-05-20 22:24:41 -0300

    implementando o metodo findall

--- helpdesk2/src/main/java/helpdesk2/resources/TecnicoResource.java
+++ helpdesk2/src/main/java/helpdesk2/resources/TecnicoResource.java
@@ -1,5 +1,8 @@
 package helpdesk2.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +25,13 @@ public class TecnicoResource {
 		Tecnico tecnico = service.findById(id);
 		
 		return ResponseEntity.ok().body(new TecnicoDto(tecnico));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<TecnicoDto>> findAll(){
+		List<Tecnico> list = service.findAll();
+		List<TecnicoDto> listDtos = list.stream().map(obj->new TecnicoDto(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDtos);
 	}
 
 }
--- helpdesk2/src/main/java/helpdesk2/services/TecnicoService.java
+++ helpdesk2/src/main/java/helpdesk2/services/TecnicoService.java
@@ -1,5 +1,6 @@
 package helpdesk2.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,11 @@ public class TecnicoService {
 	public Tecnico findById(Long id) {
 		Optional<Tecnico> obj = repository.findById(id);
 		return obj.orElseThrow(()-> new ObjectNotFundExeption("Objeto não encontrado Id: "+id));
+	}
+
+	public List<Tecnico> findAll() {
+		
+		return repository.findAll();
 	}
 
 }
